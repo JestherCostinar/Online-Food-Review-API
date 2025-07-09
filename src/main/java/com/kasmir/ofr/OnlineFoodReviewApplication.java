512fdf0d002776e272a9245cebf0c227b559c2b1
Jesther Costinar <jesther.jc15@gmail.com>
2025-07-09 13:41:17 +0800

    First commit

---
+++ src/main/java/com/kasmir/ofr/OnlineFoodReviewApplication.java
@@ -0,0 +1,13 @@
+package com.kasmir.ofr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFoodReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineFoodReviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kasmir/ofr/OnlineFoodReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kasmir.ofr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFoodReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
