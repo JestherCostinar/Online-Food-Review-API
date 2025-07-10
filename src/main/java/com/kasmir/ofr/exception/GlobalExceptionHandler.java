6c5d641e59f97fe75ea9ed3231759b4033bcd6d4
Jesther Costinar <jesther.jc15@gmail.com>
2025-07-10 16:39:15 +0800

    Add exception if email already exist

---
+++ src/main/java/com/kasmir/ofr/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.kasmir.ofr.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(EmailAlreadyExistException.class)
+    public ResponseEntity<FaultDetails> handleEmailAlreadyExistException(EmailAlreadyExistException exception,
+                                                                         WebRequest webRequest) {
+        FaultDetails faultDetails = new FaultDetails(
+                LocalDateTime.now(),
+                exception.getMessage(),
+                webRequest.getDescription(false),
+                "USER_EMAIL_ALREADY_EXIST"
+        );
+
+        return new ResponseEntity<>(faultDetails, HttpStatus.BAD_REQUEST);
+    }
+}
--- src/main/java/com/kasmir/ofr/service/UserServiceImpl.java
+++ src/main/java/com/kasmir/ofr/service/UserServiceImpl.java
@@ -3,9 +3,12 @@ package com.kasmir.ofr.service;
 import com.kasmir.ofr.dto.request.UserRequestDto;
 import com.kasmir.ofr.dto.response.UserResponseDto;
 import com.kasmir.ofr.entity.User;
+import com.kasmir.ofr.exception.EmailAlreadyExistException;
 import com.kasmir.ofr.mapper.UserMapper;
 import com.kasmir.ofr.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -18,6 +21,14 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserResponseDto createUser(UserRequestDto user) {
+        System.out.println(user.getEmail());
+        Optional<User> fetchEmail = userRepository.findByEmail(user.getEmail());
+        System.out.println(fetchEmail);
+
+        if (fetchEmail.isPresent()) {
+            throw new EmailAlreadyExistException("User Email Already exist in the record");
+        }
+
         User savedUser = UserMapper.convertToUser(user);
         UserResponseDto userResponse = UserMapper.convertToUserResponse(userRepository.save(savedUser));
         return userResponse;
